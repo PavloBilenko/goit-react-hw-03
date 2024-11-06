8686f4d269a04d2bd108ef4c3d932c048edb8067
PavloBilenko <138114488+PavloBilenko@users.noreply.github.com>
2024-11-06 22:53:19 +0100

    Initial commit

---
+++ src/components/Description/Description.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import PropTypes from "prop-types";
+import s from "./Description.module.css"
+
+const Description = () => {
+  return (
+    <div className={s.wrapper}>
+      <h1 className={s.name}>Sip Happens Café</h1>
+      <p className={s.text}>
+        Please leave your feedback about our service by selecting one of the
+        options below.
+      </p>
+    </div>
+  );
+}
+
+export default Description
+
